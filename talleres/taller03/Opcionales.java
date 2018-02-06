eb54a508c67b6f29bb9268386e886e306d838237
jflondonog <35697747+jflondonog@users.noreply.github.com>
2018-02-05 21:08:11 -0500

    Add files via upload

---
+++ talleres/taller03/Opcionales.java
@@ -0,0 +1,16 @@
+public class Opcionales
+{
+    public static void permutacionpto4(String base, String s) {
+        if(base.length() == s.length()) {
+            System.out.println(base);
+        } else {
+            for(int i=0; i<s.length(); i++) {
+                permutacionpto4(base + s.charAt(i), s);
+            }
+        }
+    }
+    
+     public static void main(String [] args) {
+        permutacionpto4("", "abc");
+    }
+}
