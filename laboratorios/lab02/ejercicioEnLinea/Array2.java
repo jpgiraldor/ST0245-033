ee90da79b9454943903d4e4e1f17f965547fc9a0
jpgiraldor <35697253+jpgiraldor@users.noreply.github.com>
2018-03-04 10:08:48 -0500

    Add files via upload

--- laboratorios/lab02/ejercicioEnLinea/Array2.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2.java
@@ -8,53 +8,53 @@
 public class Array2
 {
     public int countEvens(int[] nums) {
-        int x=0;
-        for(int i=0; i<nums.length;i++){
-            if (nums[i]%2==0)x++;
+        int x=0;                                //c1
+            for(int i=0; i<nums.length;i++){    //n
+            if (nums[i]%2==0)x++;               //c2*n
 
         }
-        return x;
+        return x;                               //c3
     }
 
     public int bigDiff(int[] nums) {
-        int x=nums[0];
-        int y=nums[0];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=x)x=nums[i]; 
-            if(nums[i]<=y)y=nums[i];
+        int x=nums[0];                          //c1
+        int y=nums[0];                          //c2
+        for(int i=0;i<nums.length;i++){         //n
+            if(nums[i]>=x)x=nums[i];            //c3*n
+            if(nums[i]<=y)y=nums[i];            //c4*n
         }
-        return x-y;
+        return x-y;                             //c5
 
     }
 
     public boolean lucky13(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==1||nums[i]==3)return false;
+        for(int i=0;i<nums.length;i++){         //n
+            if(nums[i]==1||nums[i]==3)return false; //c*n
 
         }
-        return true;
+        return true;                            //c2
     }
 
     public boolean sum28(int[] nums) {
-        int x=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==2)x++;
+        int x=0;                                //c1
+        for(int i=0;i<nums.length;i++){         //n
+            if(nums[i]==2)x++;                  //c2*n
 
         }
-        x=x*2;
-        if(x==8)return true;
-        else return false;
+        x=x*2;                                  //c3
+        if(x==8)return true;                    //c4
+        else return false;                      //c5
     }
 
     public boolean more14(int[] nums) {
-        int x=0;
-        int y=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==1)x++;
-            if(nums[i]==4)y++;
+        int x=0;                                //c1
+        int y=0;                                //c2
+        for(int i=0;i<nums.length;i++){         //n
+            if(nums[i]==1)x++;                  //c3*n
+            if(nums[i]==4)y++;                  //c4*n
 
         }
-        if (x>y)return true;
-        else return false;
+        if (x>y)return true;                    //c5
+        else return false;                      //c6
     }
 }
